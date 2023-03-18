58c565800250fe3eae5f50d445f92e6a8a2abc6d
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-03-18 15:16:11 -0700

    swapped tech stack logos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,25 @@
+// const hamburger = document.querySelector('.hamburger')
+// const navUl = document.querySelector('.nav-ul')
+
+// hamburger.addEventListener('click', function() {
+//     hamburger.classList.toggle('active')
+//     navUl.classList.toggle('active')
+// }) 
+
+// document.querySelector('.nav-a').forEach(n => n.addEventListener('click', function() {
+//     hamburger.classList.remove('active');
+//     navUl.classList.toggle('active');
+// }))
+
+
 const hamburger = document.querySelector('.hamburger')
 const navUl = document.querySelector('.nav-ul')
+const arrow = document.querySelector('.arrow')
 
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('active')
     navUl.classList.toggle('active')
+    arrow.classList.toggle('active')
 }) 
 
 document.querySelector('.nav-a').forEach(n => n.addEventListener('click', function() {
