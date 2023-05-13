31959d626ed8f295c0fdf1dfaeb27c31c28f6eba
Zach Flentge-Wong <zachflentgewong@gmail.com>
2023-05-12 23:29:01 -0700

    total revamp once again

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,25 @@
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
+
 // const hamburger = document.querySelector('.hamburger')
 // const navUl = document.querySelector('.nav-ul')
+// const arrow = document.querySelector('.arrow')
 
 // hamburger.addEventListener('click', function() {
 //     hamburger.classList.toggle('active')
 //     navUl.classList.toggle('active')
+//     arrow.classList.toggle('active')
 // }) 
 
 // document.querySelector('.nav-a').forEach(n => n.addEventListener('click', function() {
 //     hamburger.classList.remove('active');
 //     navUl.classList.toggle('active');
 // }))
-
-
-const hamburger = document.querySelector('.hamburger')
-const navUl = document.querySelector('.nav-ul')
-const arrow = document.querySelector('.arrow')
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('active')
-    navUl.classList.toggle('active')
-    arrow.classList.toggle('active')
-}) 
-
-document.querySelector('.nav-a').forEach(n => n.addEventListener('click', function() {
-    hamburger.classList.remove('active');
-    navUl.classList.toggle('active');
-}))
