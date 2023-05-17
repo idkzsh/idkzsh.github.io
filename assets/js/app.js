3877afe50b1e35fba761b380a7be512161c81f10
zach <102389340+zw0ng@users.noreply.github.com>
2023-05-16 20:46:33 -0700

    started projects section

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,17 @@ document.getElementById("cards").onmousemove = e => {
     };
   }
 
+  document.getElementById("med-cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("med-card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
+
 const slider = document.querySelector('.slides');
 const optionOne = document.querySelector('.option-1');
 const optionTwo = document.querySelector('.option-2');
@@ -20,7 +31,6 @@ optionOne.addEventListener('click', function() {
   slider.style.transform = 'translateY(0%)';
 });
 
-
 optionTwo.addEventListener('click', function() {
   slider.style.transform = 'translateY(-500px)';
 });
@@ -29,20 +39,6 @@ optionTri.addEventListener('click', function() {
   slider.style.transform = 'translateY(-1000px)';
 });
 
-// const hamburger = document.querySelector('.hamburger')
-// const navUl = document.querySelector('.links')
-// const arrow = document.querySelector('.arrow')
-
-// hamburger.addEventListener('click', function() {
-//     hamburger.classList.toggle('active')
-//     navUl.classList.toggle('active')
-//     arrow.classList.toggle('active')
-// }) 
-
-// document.querySelector('.links-a').forEach(n => n.addEventListener('click', function() {
-//     hamburger.classList.remove('active');
-//     navUl.classList.toggle('active');
-// }))
 
 const primaryNav = document.querySelector('.links')
 const hamburger = document.querySelector('.hamburger')
