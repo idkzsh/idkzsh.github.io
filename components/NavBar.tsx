9ff7e9fa78fe1f4d7db315a80181be96b1c3d8f7
zach <102389340+zw0ng@users.noreply.github.com>
2023-09-17 15:37:42 -0700

    fix: - bg was applied to wrong div in navbar, updated to span whole screen

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,14 +10,16 @@ const NavBar = () => {
   return (
     <>
       <div className="z-20 fixed flex w-full h-[7vh] justify-center items-center">
-        <div className="flex items-center max-w-[1000px] w-[100%] justify-between p-4 bg-custom-gradient">
-          <Logo />
-          {path === "/" ? (
-            ""
-          ) : (
-            <span className="z-10 text-xs">{path.slice(1)}</span>
-          )}
-          <Toggle />
+        <div className="z-20 flex items-center justify-center w-full h-full bg-custom-gradient">
+          <div className="flex items-center max-w-[1000px] w-[100%] justify-between p-4 ">
+            <Logo />
+            {path === "/" ? (
+              ""
+            ) : (
+              <span className="z-10 text-xs">{path.slice(1)}</span>
+            )}
+            <Toggle />
+          </div>
         </div>
       </div>
     </>
