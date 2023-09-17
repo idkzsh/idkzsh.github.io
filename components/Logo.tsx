3c1203ae04aa196fef01410b967db1329e543740
zach <102389340+zw0ng@users.noreply.github.com>
2023-09-17 15:33:53 -0700

    feat: - updated to ? logo - changed bg coloring slightly - updated transparency of light mode cards

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,8 +5,8 @@ const Logo = () => {
   return (
     <>
       <Link href="/#" className="z-10">
-        <div className="flex items-center justify-center w-10 h-10 text-xs font-bold duration-300 rounded-sm hover:-rotate-6 hover:scale-90 bg-primary text-background">
-          idkz
+        <div className="flex items-center justify-center w-8 h-8 text-xs font-bold duration-300 rounded-sm hover:-rotate-6 hover:scale-90 bg-primary text-background">
+          ?
         </div>
       </Link>
     </>
