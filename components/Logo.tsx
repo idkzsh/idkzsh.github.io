a27648aef034a94684ffd9fbebac6d6ad42a2af0
zach <102389340+zw0ng@users.noreply.github.com>
2023-09-17 17:02:57 -0700

    fix: - logo sizing udpated - new favicon

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
   return (
     <>
       <Link href="/#" className="z-10">
-        <div className="flex items-center justify-center w-8 h-8 text-xs font-bold duration-300 rounded-sm hover:-rotate-6 hover:scale-90 bg-primary text-background">
+        <div className="flex items-center justify-center w-8 h-8 text-2xl font-bold duration-300 rounded-sm hover:-rotate-6 hover:scale-90 bg-primary text-background">
           ?
         </div>
       </Link>
