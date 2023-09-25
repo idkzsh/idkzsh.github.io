d88eb0c3ec60cd75c1cd7b8c59e616265bea9600
zach <102389340+zw0ng@users.noreply.github.com>
2023-09-24 17:27:49 -0700

    fix: - project card was also imported on film page

--- app/film/page.tsx
+++ app/film/page.tsx
@@ -1,11 +1,11 @@
-import ProjectCard from '@/components/project-card'
+import LinkCard from '@/components/link-card'
 import React from 'react'
 
 const Film = () => {
   return (
     <div className="gap-4 flex flex-col items-center justify-center w-[80vw] max-w-[800px] h-[70vh] mb-11 z-10">
     <div className="flex flex-wrap justify-center w-full gap-3">
-      <ProjectCard title="coming soon" />
+      <LinkCard title="coming soon" />
     </div>
   </div>
   )
