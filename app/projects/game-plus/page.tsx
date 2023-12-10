6a954ed865b56593988e512f24b3f8a7f30634e0
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-09 23:26:14 -0800

    added link to gameplus project

--- app/projects/game-plus/page.tsx
+++ app/projects/game-plus/page.tsx
@@ -1,4 +1,7 @@
 import Spacer from "@/components/Spacer";
+import LinkCard from "@/components/link-card";
+import Link from "next/link";
+import {IoIosAddCircle} from 'react-icons/io'
 
 const GamePlus = () => {
   return (
@@ -6,7 +9,14 @@ const GamePlus = () => {
       <Spacer height="h-[200px]" />
       <div className="z-10 max-w-[1000px] w-[90%] h-[100%] flex flex-wrap-reverse mb-4">
         <div className="flex flex-col w-[49%] min-w-[330px] gap-3">
-          <h1 className="text-3xl font-bold">Image Renaming Tool</h1>
+          <h1 className="text-3xl font-bold">GamePlus</h1>
+          <Link
+            target="_blank"
+            href="https://grand-monstera-b8f151.netlify.app/"
+          >
+            <LinkCard title="View Project" 
+            icon={<IoIosAddCircle/>}/>
+          </Link>
         </div>
       </div>
     </>
