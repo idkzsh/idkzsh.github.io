88dfe03b302973deb792d3c2d30a40b97c6dfb82
zach <102389340+zw0ng@users.noreply.github.com>
2023-12-19 20:06:28 -0800

    on mobile, site navigates to links like linktree, desktop navigates to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
+"use client";
 import Home from "@/components/Home";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default async function Index() {
+export default function Index() {
+  const [count, setCount] = useState(0);
+  const router = useRouter();
+
+  const reRoute = () => {
+    if (count > 0) {
+      return;
+    } else {
+      router.push("/links");
+      setCount(count + 1);
+    }
+  };
+
+  const isMobile = () => {
+    return window.innerWidth < 768;
+  };
 
   return (
-    <div className="w-full h-[70vh]  flex flex-col items-center justify-center">
+    <>
+      {isMobile() ? (
+        reRoute()
+      ) : (
+        <div className="w-full h-[70vh] flex flex-col items-center justify-center">
           <Home />
-    </div>
-  )
+        </div>
+      )}
+    </>
+  );
 }
