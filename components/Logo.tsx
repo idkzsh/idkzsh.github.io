5937b60b8105fef45f053548e0408bc9d9cf327e
zach <102389340+zw0ng@users.noreply.github.com>
2024-01-22 20:07:08 -0800

    more logo adjustments

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -5,11 +5,12 @@ const Logo = () => {
   return (
     <>
       <Link href="/#" className="z-10">
-      <div
-        className="flex justify-center items-center w-[30px] h-[30px] bg-primary rounded-lg duration-300 text-background font-bold text-xl cursor-pointer hover:-rotate-12 hover:scale-90 z-100"
-      >
-        ?
-      </div>
+        <div
+          className="flex justify-center items-center w-[30px] h-[30px] bg-primary rounded-lg 
+        duration-300 text-background font-bold text-lg cursor-pointer hover:-rotate-12 hover:scale-90 z-100"
+        >
+          ?
+        </div>
       </Link>
     </>
   );
