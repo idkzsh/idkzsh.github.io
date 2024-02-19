2dda0241500d0f13b586164165d23cdd36aebf6d
zach <102389340+zw0ng@users.noreply.github.com>
2024-02-18 18:18:53 -0800

    removing node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 import htmx from "astro-htmx";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), htmx()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+  integrations: [tailwind(), htmx()]
 });
