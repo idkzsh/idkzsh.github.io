86534cad6ff03ef83b61d015c36f9b2f1f3e192a
zach <zachflentgewong@gmail.com>
2025-02-13 21:18:48 -0800

    redo portfolio

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource hono/jsx */
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
+import { logger } from 'hono/logger'
+import { jsxRenderer } from 'hono/jsx-renderer'
+import { Layout } from './views/Layout'
+import { Home } from './views/components/Home'
+
+const app = new Hono()
+
+// Middleware
+app.use('*', logger())
+app.use('/dist/*', serveStatic({ root: './' }))
+app.use('*', jsxRenderer(({ children }) => {
+  return <Layout>{children}</Layout>
+}))
+
+app.get('/', (c) => {
+  return c.render(<Home />)
+})
+
+app.get('/api/time', (c) => {
+  return c.text(`Current time: ${new Date().toLocaleTimeString()}`)
+})
+
+export default app
